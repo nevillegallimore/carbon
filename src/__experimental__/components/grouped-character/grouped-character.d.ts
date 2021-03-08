5bbf9c7ba10a71c5a5e391903228ff8e52357f00
grabkowski <jakub.grabkowski@sage.com>
2021-03-08 16:23:54 +0100

    refactor(grouped-character): extend textboxprops and remove redundand type definitions

--- src/__experimental__/components/grouped-character/grouped-character.d.ts
+++ src/__experimental__/components/grouped-character/grouped-character.d.ts
@@ -1,20 +1,14 @@
 import * as React from "react";
+import { TextboxProps } from "../textbox";
 
-export interface GroupedCharacterProps {
-  /** character to be used as separator - has to be a 1 character string */
-  separator?: string;
-  /** pattern by which input value should be grouped */
-  groups?: any[];
-  /** Input value if component is meant to be used as a controlled component */
-  value?: string;
+export interface GroupedCharacterProps extends TextboxProps {
   /** Default input value if component is meant to be used as an uncontrolled component */
   defaultValue?: string;
-  /** on change handler which receives the event with object as a value containing rawValue and formattedValue */
-  onChange?: (ev: React.ChangeEvent<HTMLElement>) => void;
-  /** on blur handler which receives the event with object as a value containing rawValue and formattedValue */
-  onBlur?: (ev: React.ChangeEvent<HTMLElement>) => void;
-  /** Flag to configure component as mandatory */
-  required?: boolean;
+  /** pattern by which input value should be grouped */
+  groups?: any[];
+  /** character to be used as separator - has to be a 1 character string */
+  separator?: string;
+
 }
 
 declare const GroupedCharacter: React.FunctionComponent<GroupedCharacterProps>;
