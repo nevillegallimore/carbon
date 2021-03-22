ce55f1c54bd834d4c5c54bff284147088b3b5b3f
Michał Maciorowski <michal.maciorowski2@sage.com>
2021-03-22 12:36:10 +0100

    refactor(welcome-page): remove i18n-js

--- src/components/form/form.spec.js
+++ src/components/form/form.spec.js
@@ -18,6 +18,8 @@ import Icon from "../icon";
 import Button from "../button";
 import { FieldsetStyle } from "../../__experimental__/components/fieldset/fieldset.style";
 
+jest.mock("lodash/debounce", () => jest.fn((fn) => fn));
+
 describe("Form", () => {
   let wrapper;
 
