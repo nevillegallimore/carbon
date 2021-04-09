728b392b721b482537ffb08338f5a1aa20f4cb30
grabkowski <jakub.grabkowski@sage.com>
2021-04-09 14:00:49 +0200

    feat(inline-inpouts): add ts type definitions

---
+++ src/components/inline-inputs/inline-inputs.d.ts
@@ -0,0 +1,22 @@
+import * as React from "react";
+import { IconProps } from "../icon/icon";
+import * as OptionsHelper from "../../utils/helpers/options-helper/options-helper";
+
+export interface InlineInputsProps {
+  /** Children elements */
+  children: React.ReactElement<IconProps>;
+  /** [Legacy prop] A custom class name for the component. */
+  className?: string;
+  /** Gutter prop gets passed down to Row component if false gutter value is "none" */
+  gutter?: "none" | OptionsHelper.SizesFull;
+  /** The id of the corresponding input control for the label */
+  htmlFor?: string;
+  /** Defines the label text for the heading. */
+  label?: string;
+  /** Callback */
+  onAction: React.MouseEventHandler<HTMLButtonElement>;
+}
+
+declare function InlineInputs(props: InlineInputsProps): JSX.Element;
+
+export default InlineInputs;
